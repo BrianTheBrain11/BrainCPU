a26be7d93d8fae988c37cd0f5ff76ca5364c68a4
BrianTheBrain11 <68790720+BrianTheBrain11@users.noreply.github.com>
2021-05-03 12:21:44 -0700

    Worked on some core audio stuff
    still need to assign the memory map

--- Brain/AudioCore.cs
+++ Brain/AudioCore.cs
@@ -8,6 +8,27 @@ namespace Brain
 {
     class AudioCore
     {
+        ushort audioMemPointer;
+        
         byte[] audioMemory = new byte[1024];
+        
+        void AudioInitialize()
+        {
+            audioMemPointer = 0x00;
+        }
+        
+        void AudioLoop()
+        {
+            if (audioMemory[audioMemPointer])
+            {
+                audioMemPointer = 0x00;
+                
+                //check memory flag for audio stop/swap, stop or load audio memory if so
+                
+                return;
+            }
+            PlayAudio(audioMemory[audioMemPointer]);
+            audioMemPointer++;
+        }
     }
 }
